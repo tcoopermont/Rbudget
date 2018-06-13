bfea7c57312f80debfefa6d103f49f405a153039
tom cooper <tomcooper@tec8.net>
2018-06-13 05:37:31 -0700

    removed last element

--- merge-by-id.R
+++ merge-by-id.R
@@ -19,15 +19,23 @@ budget <- read_ods(budget_file,col_types=cols(col_character(),
 					      col_character(),
 					      col_character(),
 					      col_integer()))
+budget <- separate(budget,c("t1","t2","t3","t4","t5","t6"),",") %>%
+	mutate(Transaction.ID = unite(t1,t2,t3,t4,t5,sep=","))
+
 str(budget)
 #cat_lookups <- budget$lookup
 #budget <- select(budget,-lookup)
 
 new_trans <- read.csv(source_file)
 
+#try and remove the last element of transaction id, hope it'll be enough
 new_trans <- filter(new_trans,Transaction.Type == "Debit") %>%
 	select(Transaction.ID,Posting.Date,Amount,Description) %>%
-	mutate(Posting.Date = as.Date(Posting.Date,format='%m/%d/%Y'),category = "",period=NA)
+	separate(Transaction.ID,c("t1","t2","t3","t4","t5","t6"),",") %>%
+	mutate(Transaction.ID = unite(t1,t2,t3,t4,t5,sep=",")
+	       Posting.Date = as.Date(Posting.Date,format='%m/%d/%Y'),
+	       category = "",period=NA) 
+	
 
 head(new_trans)
 new_trans_append <- filter(new_trans,! Transaction.ID %in% budget$Transaction.ID)
